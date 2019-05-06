5472c191eb0406cd10993fc25fb7d20fbcec548e
Kongaman <christian.kranz@gmail.com>
2019-05-06 21:28:18 +0200

    tests always pass if there is no check

--- test/com/ck/junit5/StringTest.java
+++ test/com/ck/junit5/StringTest.java
@@ -7,8 +7,12 @@ import org.junit.jupiter.api.Test;
 class StringTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void whatever() {
+		int length = "ABCD".length();
+		// we want to assert length == 4
+		// BUT: There is no check, so the test will succeed even if the length()-Method
+		// would return 5 or anything else!!!
+		// run as -> junit test --> green bar = success
 	}
 
 }
